760d6126097719610017487397f86aa06d56fc51
AffinityChaos <mrmsmanjimehta@gmail.com>
2025-08-03 22:29:17 +0530

    testimonial section added

--- dist/js/swiper.js
+++ dist/js/swiper.js
@@ -1,32 +1,32 @@
 const swiper = new Swiper('.aboutUsSwiper', {
-    direction: 'vertical',
-    loop: true,
-    effect: 'fade',
-    speed: 6000,
-    grabCursor: true, // corrected: 'grabbingCursor' → 'grabCursor'
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-    },
-    fadeEffect: {
-        crossFade: true, // smooth fade transition
-    },
+  direction: 'vertical',
+  loop: true,
+  effect: 'fade',
+  speed: 6000,
+  grabCursor: true, // corrected: 'grabbingCursor' → 'grabCursor'
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  fadeEffect: {
+    crossFade: true, // smooth fade transition
+  },
 });
 
 const Heroswiper = new Swiper('.heroSwiper', {
-    loop: true,
-    // effect: 'fade',
-    grabCursor: true, // corrected: 'grabbingCursor' → 'grabCursor'
-    // autoplay: {
-    //     delay: 4000,
-    // },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    fadeEffect: {
-        crossFade: true, // smooth fade transition
-    },
+  loop: true,
+  // effect: 'fade',
+  grabCursor: true, // corrected: 'grabbingCursor' → 'grabCursor'
+  // autoplay: {
+  //     delay: 4000,
+  // },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  fadeEffect: {
+    crossFade: true, // smooth fade transition
+  },
 });
 
 const category = new Swiper('.categorySwiper', {
@@ -34,30 +34,61 @@ const category = new Swiper('.categorySwiper', {
   direction: 'horizontal',
   loop: true,
   slidesPerView: 1,
-  
+
   autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
+    delay: 3000,
+    disableOnInteraction: false,
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  
+
   breakpoints: {
-        564:{
-          slidesPerView:1,
-        },
-        // when window width is >= 768px
-        768: {
-          slidesPerView: 2
-        },
-        // when window width is >= 1024px
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        }
-      }
+    564: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
+const testimonial = new Swiper('.testimonialSwiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+  breakpoints: {
+    564: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 });
